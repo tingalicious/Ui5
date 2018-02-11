fc539317d114ad294fce8c1781c1685bf38482ce
Ying-Ting Chen <tingy0314@gmail.com>
2018-02-11 11:44:02 -0600

    intialization of component
    Wrapped core webapp functions into the component

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,19 +1,17 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
+   "sap/ui/model/resource/ResourceModel",
    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
    "use strict";
    return Controller.extend("Ui5.controller.App", {
-      onInit : function() {
-        var oData = {
-          recipient : {
-            name: "World"
-          }
-        };
-      },
       onShowHello : function () {
-         MessageToast.show("Hello World");
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        //show message
+         MessageToast.show(sMsg);
       }
    });
 });
