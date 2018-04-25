9246a8793b0c3b50584b033d7a22fbac1d3ac503
Ying-Ting Chen <tingy0314@gmail.com>
2018-04-25 05:30:30 -0500

    Added Manifest
    manifest.json lol

--- Component.js
+++ Component.js
@@ -6,12 +6,7 @@ sap.ui.define([
    "use strict";
    return UIComponent.extend("Ui5.Component", {
             metadata : {
-		rootView: {
-			"viewName": "Ui5.view.App",
-			"type": "XML",
-			"async": true,
-			"id": "app"
-		}
+		            manifest: "json"
 	},
       init : function () {
          // call the init function of the parent
